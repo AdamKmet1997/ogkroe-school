95d88b1ace8e0d84e345a744a3ecad4092e7f533
adamkmet_dev <adamkmet_dev@outlook.com>
2022-07-08 18:23:00 +0100

    add more pages

--- ogkroe_school/urls.py
+++ ogkroe_school/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', include('account.urls')),
     path('account', include("django.contrib.auth.urls")),
     path('account', include('account.urls')),
-    path('school/', include('school.urls')),
-    path('ogkroe/', include('ogkroe_youtube.urls')),
+    path('academy/', include('school.urls')),
+    path('youtube/', include('ogkroe_youtube.urls')),
     path('admin/', admin.site.urls),
 ]
