0fe7867f8e607d77b39a3cdcc6648a8e03f41c89
adamkmet_dev <adamkmet_dev@outlook.com>
2022-07-13 09:08:50 +0100

    db host localhosts debug false

--- ogkroe_school/settings.py
+++ ogkroe_school/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yg(zeg6wretqv!8%#ikeq4a)p@4kto%cp2hp-me5o(^&+pxv2p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME':'postgres',
         'USER': 'postgres',
         'PASSWORD': 'Angie1997',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 
