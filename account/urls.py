560d621ed9520776ec09b1162af360e0b393f213
Gab-S <lolgab2010@yahoo.ie>
2022-07-13 19:30:59 +0100

    fix urls pattern warning

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/signup/',views.user_signup, name='signup'),
-    path('/login/', views.user_login, name='login'),
+    path('signup/',views.user_signup, name='signup'),
+    path('login/', views.user_login, name='login'),
 ]
--- ogkroe_school/urls.py
+++ ogkroe_school/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('school.urls')),
     path('account', include("django.contrib.auth.urls")),
-    path('account', include('account.urls')),
+    path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
