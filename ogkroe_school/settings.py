f7b10c361a3570c550e205cc417f06605bf55f89
adamkmet_dev <adamkmet_dev@outlook.com>
2022-07-13 09:14:30 +0100

    add sqlite

--- ogkroe_school/settings.py
+++ ogkroe_school/settings.py
@@ -77,19 +77,24 @@ WSGI_APPLICATION = 'ogkroe_school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Angie1997',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+
+# }
 DATABASES = {
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Angie1997',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
