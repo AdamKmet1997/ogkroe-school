990ecefe7a5603c892cfadce5ca20e7e779e1d4f
adamkmet_dev <adamkmet_dev@outlook.com>
2022-07-13 09:23:08 +0100

    debug true

--- ogkroe_school/settings.py
+++ ogkroe_school/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yg(zeg6wretqv!8%#ikeq4a)p@4kto%cp2hp-me5o(^&+pxv2p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
